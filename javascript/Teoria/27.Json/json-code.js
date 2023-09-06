96700ebc7fa6899b2ae2205103bfbb09b0c10785
Lidia Garcia Muñoz <lidiagarmu@gmail.com>
2023-09-06 19:12:05 +0200

    this y json

--- javascript/Teoria/27.Json/json-code.js
+++ javascript/Teoria/27.Json/json-code.js
@@ -1,23 +1,20 @@
-// METODO json es un formato de lectura de datos y lo devuelve en formato de string. Es de uso diario
+/* METODO json es un formato de lectura de datos y lo devuelve en formato de string. Es de uso diario. Es decir, es un formato
+de texto que se usa para representar datos estructurados en la forma de objetos*/
 
-const myUsers = { name: "Jhon", age: 30 };
+const myUsers = { name: "Alice", age: 40 };
 
 console.log(myUsers); //muestra clave y valor pintado de color, clave y valor . No lo muestra en forma de string
 
 console.log(JSON.stringify(myUsers)); // al escribir JSON. estoy entrando en el tipo
-//muestra en terminal --> {"name":"Jhon","age":30} en forma de string
-
-// Y, ¿como sabemos que es en formato string? Vease en FILA 5 como no muestra en forma string
+//muestra en terminal --> {"name":"Alice","age":50} en forma de string
 
 // Podria ser:
 
 console.log(JSON.stringify({ name: "Peter", age: 10 })); // muestra tambien en formato String
 
-//--------
 
-const json = '{"name":"Alice", "age":30,"city":"New York"}';
-
-const obj = JSON.parse(json);
-
-console.log(obj); // nos lo devuelve como si fuera un objeto. Nos quita las comillas. Al igual que en FILA 5
-//El método JSON. parse() analiza una cadena de texto como JSON, transformando opcionalmente el valor producido por el análisi
+/* Un objeto JSON se representa como una serie de pares clave-valor separados por comas, con las claves entre comillas dobles
+y los valores pueden ser de cualquier tipo, incluyendo numeros, cadenas de texto, booleanos o incluso otro objeto o matriz.
+      -   Los datos estan definidos mediante nombre/valor
+      -   Los datos estan separados por comas
+      -   Las llaves contienen objetos. 
