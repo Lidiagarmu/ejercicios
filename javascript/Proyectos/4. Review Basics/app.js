310e623392cedca7cc7841631d286bd71024edc5
Lidia Garcia Muñoz <lidiagarmu@gmail.com>
2023-09-12 20:48:19 +0200

    ejercicios review basics para hacer

--- javascript/Proyectos/4. Review Basics/app.js
+++ javascript/Proyectos/4. Review Basics/app.js
@@ -1,127 +1,128 @@
-/* **Iteración #1: Buscar el máximo**
+/* **Iteración #1: Mix for e includes**
+Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las 
+categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función **.includes()** */
 
-Completa la función que tomando dos números como argumento devuelva el más alto. */
-
-function sum(numberOne, numberTwo) {
-  // insert code
-}
-
-/* **Iteración #2: Buscar la palabra más larga**
-
-Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
-
-Puedes usar este array para probar tu función:
-  
-  */
-
-const avengers = [
-  "Hulk",
-  "Thor",
-  "IronMan",
-  "Captain A.",
-  "Spiderman",
-  "Captain M.",
+const movies = [
+  { title: "Madaraspar", duration: 192, categories: ["comedia", "aventura"] },
+  { title: "Spiderpan", duration: 122, categories: ["aventura", "acción"] },
+  {
+    title: "Solo en Whatsapp",
+    duration: 223,
+    categories: ["comedia", "thriller"],
+  },
+  {
+    title: "El gato con guantes",
+    duration: 111,
+    categories: ["comedia", "aventura", "animación"],
+  },
 ];
-function findLongestWord(param) {
-  // insert code
-}
 
-/* **Iteración #3: Calcular la suma**
+/* **Iteración #2: Mix Fors**
 
-Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
-Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. 
+Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios. */
 
-Puedes usar este array para probar tu función: */
-
-const numbers = [1, 2, 3, 5, 45, 37, 58];
-
-function sumAll(param) {
-  // insert code
-}
-
-/* **Iteración #4: Calcular el promedio**
-
-Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función: */
-
-const numbers2 = [12, 21, 38, 5, 45, 37, 6];
-function average(param) {
-  // insert code
-}
-
-/* **Iteración #5: Calcular promedio de strings**
-
-Crea una función que reciba por parámetro un array y cuando es un valor number lo sume 
-y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función: */
-
-const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
-function averageWord(param) {
-  // insert code
-}
-
-/* **Iteración #6: Valores únicos**
-
-Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que 
-existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar
- tu función: */
-
-const duplicates = [
-  "sushi",
-  "pizza",
-  "burger",
-  "potatoe",
-  "pasta",
-  "ice-cream",
-  "pizza",
-  "chicken",
-  "onion rings",
-  "pasta",
-  "soda",
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
 ];
-function removeDuplicates(param) {
-  // insert code
-}
 
-/* **Iteración #7: Buscador de nombres**
+/* **Iteración #3: Mix Fors**
 
-Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro 
-de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición 
-de dicho elemento y por la contra un false. Puedes usar este array para probar tu función */
+Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
 
-const nameFinder = [
-  "Peter",
-  "Steve",
-  "Tony",
-  "Natasha",
-  "Clint",
-  "Logan",
-  "Xabier",
-  "Bruce",
-  "Peggy",
-  "Jessica",
-  "Marc",
+Este ejercicio es un poco complicado con los conocimientos actuales pero...a la vez un buen reto y oportunidad para comprender que hay muchas formas de hacer las cosas en javascript. */
+
+const users2 = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
 ];
-function finderName(param) {
-  // insert code
-}
 
-/* **Iteration #8: Contador de repeticiones**
+/*  **Iteración #4: Métodos findArrayIndex**
 
-Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma. 
- Puedes usar este array para probar tu función:*/
+Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
 
-const counterWords = [
-  "code",
-  "repeat",
-  "eat",
-  "sleep",
-  "code",
-  "enjoy",
-  "sleep",
-  "code",
-  "enjoy",
-  "upgrade",
-  "code",
+Sugerencia de función:  */
+
+function findArrayIndex(array, text) {}
+
+// Ej array:
+
+["Caracol", "Mosquito", "Salamandra", "Ajolote"][
+  /* **Iteración #5: Función rollDice**
+
+Crea una función llamada **rollDice()** que reciba como parametro el numero de caras 
+que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la 
+función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como 
+hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();**  */
+
+  /* **Iteración #6: Función swap**
+
+Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. 
+La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. 
+Retorna el array resultante.
+
+Sugerencia de array:  */
+
+  ("Mesirve", "Cristiano Romualdo", "Fernando Muralla", "Ronalguiño")
 ];
-function repeatCounter(param) {
-  // insert code
-}
